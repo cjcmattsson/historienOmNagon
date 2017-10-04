8317b8befac9e68ba0b62b14637b909ffa6fd133
cjcmattsson <cjcmattsson@gmail.com>
2017-10-04 19:43:10 +0200

    ÄNDRADE MASSA
    Ändrat massvis

---
+++ Typewriter/scroll.js
@@ -0,0 +1,15 @@
+
+// KODEN NEDAN STÄNGER AC MÖJLIGHETEN ATT SCROLLA MED MUS/TOUCHPAD
+// window.onwheel = function(){ return false; }
+
+$(document).ready(function() {
+    $(".scroll-down").click(function(event){
+        $('html, body').animate({scrollTop: '+=400px'}, 800);
+    });
+});
+
+$(document).ready(function() {
+    $(".scroll-up").click(function(event){
+        $('html, body').animate({scrollTop: '-=400px'}, 800);
+    });
+});
